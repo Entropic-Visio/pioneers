8954edd0b62fa7be388fb18e1ba59b3753d799d3
Entropic-Visio <entropicvisio@gmail.com>
2024-04-11 05:07:51 +0100

    globe.js updated

--- src/public/scripts/globe.js
+++ src/public/scripts/globe.js
@@ -1,11 +1,15 @@
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x388659);
+
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 4;
 camera.position.x = 2;
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth*0.98, window.innerHeight*0.98);
 document.body.appendChild(renderer.domElement);
+
+renderer.setClearColor(scene.background, 1);
 
 window.addEventListener('resize', () => {
     const width = window.innerWidth;
@@ -28,7 +32,7 @@ const particleGeometry = new THREE.BufferGeometry();
 
 const positions = [];
 
-for (let i = 0; i < 1000; i++) {
+for (let i = 0; i < 1600; i++) {
     const vertex = new THREE.Vector3();
     vertex.x = Math.random() * 2 - 1;
     vertex.y = Math.random() * 2 - 1;
